1a5c16e978db5bfeea25d9776d2f60579da3a217
JaKristan Cavitt <jakristan.cavitt@gmail.com>
2026-08-04 13:56:27 -0500

    Fix GitHub Pages asset paths

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  // GitHub Pages serves this project from:
+  // /react-portfolio/
+  base: "/react-portfolio/",
+});
